57a84aa5a6e185cb90fde6cdc05fd46d33bccee3
espoupou <126259211+espoupou@users.noreply.github.com>
2024-03-15 16:46:40 +0000

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ pascal triangle implementation """
 
+
 def pascal_triangle(n):
     """ pascal function """
 
@@ -15,4 +16,3 @@ def pascal_triangle(n):
                 sum(triangle[i-1][j:j+2]))
         triangle[i].append(1)
     return triangle[:n]
-
